4cb0ad5c976e0ed0edfdd328f0658bf6a8f513ee
Sašo Stanovnik <saso.stanovnik@xlab.si>
2019-03-07 11:36:07 +0100

    Specify all auth plugin options in the auth config file.

--- entrypoint-mosquitto.sh
+++ entrypoint-mosquitto.sh
@@ -5,10 +5,14 @@ set -e
 set -u
 
 echo "Configuring mosquitto to use the HTTP auth backend at ${MOSQUITTO_AUTH_HTTP_HOST}:${MOSQUITTO_AUTH_HTTP_PORT}"
-cat >/etc/mosquitto/mosquitto.conf.d/mosquitto-auto.conf <<EOF
+cat >/etc/mosquitto/mosquitto.conf.d/mosquitto-auth.conf <<EOF
+# the auth_plugin option must be specified here lest mosquitto complains
+# so just specify everything here
+auth_plugin /usr/local/lib/auth-plug.so
+auth_opt_backends http
 auth_opt_http_ip            ${MOSQUITTO_AUTH_HTTP_HOST}
 auth_opt_http_port          ${MOSQUITTO_AUTH_HTTP_PORT}
-# auth_opt_http_hostname
+auth_opt_http_hostname      ${MOSQUITTO_AUTH_HTTP_HOST}
 auth_opt_http_getuser_uri   /auth
 auth_opt_http_superuser_uri /superuser
 auth_opt_http_aclcheck_uri  /acl
